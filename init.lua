2ce7e05208231740591c2890afefac0bf9e9490b
Marco Kuoni <marco@lemonbrain.ch>
2025-04-01 18:09:32 +0200

    current state

--- init.lua
+++ init.lua
@@ -1,2 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Only run once on first file load
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  once = true,
+  callback = function()
+    local file_dir = vim.fn.expand("%:p:h")
+    if vim.fn.isdirectory(file_dir) == 1 then
+      vim.cmd("cd " .. vim.fn.fnameescape(file_dir))
+    end
+  end,
+})
